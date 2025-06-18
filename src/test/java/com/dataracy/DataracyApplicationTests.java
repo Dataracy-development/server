eec5b7ff02b211b78665f7839de83e4ee8bbb6a9
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-18 11:17:17 +0900

    #2 [chore] 스프링부트 초기세팅을 진행한다.

---
+++ src/test/java/com/dataracy/DataracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
