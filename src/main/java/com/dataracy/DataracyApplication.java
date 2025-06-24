718c321117f1ebeaf0459e6bdb205f89167f0a83
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-24 13:34:24 +0900

    #53 [fix] SpringBootApplication에 컴포넌트 스캔관련 코드를 삭제한다.

--- src/main/java/com/dataracy/DataracyApplication.java
+++ src/main/java/com/dataracy/DataracyApplication.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(
-		scanBasePackages = {
-		"com.dataracy",
-		"cores.user_module.config"
-	}
-)
+@SpringBootApplication
 public class DataracyApplication {
 
 	public static void main(String[] args) {
