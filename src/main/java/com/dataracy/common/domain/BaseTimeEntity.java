c004c50891f27ad6e7c18dad03567da5c9a8e5f9
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 14:48:45 +0900

    #81 [style] auditing 기능에 대한 주석 설명을 추가한다.

--- src/main/java/com/dataracy/common/domain/BaseTimeEntity.java
+++ src/main/java/com/dataracy/common/domain/BaseTimeEntity.java
@@ -10,7 +10,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-// 날짜만
+/**
+ * 엔티티 생성 및 수정 시 자동으로 시간 정보를 관리하기 위한 클래스입니다.
+ * 해당 클래스를 상속받는 경우, @EntityListeners를 통해
+ * JPA Auditing 기능이 적용되어 자동으로 'createdAt'과 'updatedAt' 필드가
+ * 관리됩니다.
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
