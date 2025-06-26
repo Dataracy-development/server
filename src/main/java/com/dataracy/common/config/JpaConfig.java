6f59f4de4fb1a4f6509612a3189f0640fc8b51da
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 14:43:13 +0900

    #81 [feat] SRP 원칙에 의해 auditing 관련 설정을 변경한다.

--- src/main/java/com/dataracy/common/config/JpaConfig.java
+++ src/main/java/com/dataracy/common/config/JpaConfig.java
@@ -1,9 +1,32 @@
 package com.dataracy.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+
+//    @Bean
+//    public AuditorAware<String> auditorProvider() {
+//        return () -> {
+//            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//            // 인증되지 않은 사용자 처리 (익명 사용자 또는 비로그인)
+//            if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+//                return Optional.of("anonymous");
+//            }
+//
+//            // 실무 기준: 유저 ID (Long)을 문자열로 반환
+//            Object principal = authentication.getPrincipal();
+//
+//            if (principal instanceof CustomUserDetails userDetails) {
+//                return Optional.of(String.valueOf(userDetails.getUserId()));
+//            }
+//            //예외 상황 대응
+//            return Optional.of("unknown");
+//        };
+//    }
 }
