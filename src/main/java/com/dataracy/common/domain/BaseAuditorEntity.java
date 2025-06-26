da3b9fc38e75cb5a01bb08f2c22e426980a85c59
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 14:43:13 +0900

    #81 [feat] SRP 원칙에 의해 auditing 관련 설정을 변경한다.

---
+++ src/main/java/com/dataracy/common/domain/BaseAuditorEntity.java
@@ -0,0 +1,23 @@
+package com.dataracy.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+// 작성자만
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseAuditorEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}
