6578befe3cd430200aa79729074df93f3cf37c76
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-27 00:41:49 +0900

    #85 [fix] CommonException은 도메인 관련 에러를 처리하는 BusinessException이 아닌 공통적인 예외상황들을 가리키므로 CustomException을 상속하도록 변경한다.

--- src/main/java/com/dataracy/common/status/CommonException.java
+++ src/main/java/com/dataracy/common/status/CommonException.java
@@ -1,12 +1,12 @@
 package com.dataracy.common.status;
 
-import com.dataracy.common.exception.BusinessException;
+import com.dataracy.common.exception.CustomException;
 
 /**
  * GlobalException은 애플리케이션 내에서 공통 규칙 위반을 나타내는
  * 커스텀 기본 클래스입니다.
  */
-public class CommonException extends BusinessException {
+public class CommonException extends CustomException {
     public CommonException(BaseErrorCode errorCode){
         super(errorCode);
     }
