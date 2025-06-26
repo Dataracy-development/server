41b24fafb0e590b01cd91cb7f8140fd831cd4fd2
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 14:57:28 +0900

    #81 [fix] 응답 DTO에서 Spring에 의존적인 HttpStatus에 의존하지 않도록 하여 특정 프레임워크에 의존적이지 않게 한다.

--- src/main/java/com/dataracy/common/dto/ErrorResponse.java
+++ src/main/java/com/dataracy/common/dto/ErrorResponse.java
@@ -3,19 +3,18 @@ package com.dataracy.common.dto;
 import com.dataracy.common.status.BaseErrorCode;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @Builder
 public class ErrorResponse {
 
-    private HttpStatus httpStatus;
+    private int httpStatus;
     private String code;
     private String message;
 
     public static ErrorResponse of(BaseErrorCode errorCode) {
         return ErrorResponse.builder()
-                .httpStatus(errorCode.getHttpStatus())
+                .httpStatus(errorCode.getHttpStatus().value())
                 .code(errorCode.getCode())
                 .message(errorCode.getMessage())
                 .build();
@@ -23,7 +22,7 @@ public class ErrorResponse {
 
     public static ErrorResponse of(BaseErrorCode errorCode, String customMessage) {
         return ErrorResponse.builder()
-                .httpStatus(errorCode.getHttpStatus())
+                .httpStatus(errorCode.getHttpStatus().value())
                 .code(errorCode.getCode())
                 .message(customMessage)
                 .build();
