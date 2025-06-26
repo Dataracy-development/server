2287ddb19ae014bb1934341dab00e4321e4bbfde
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 20:25:02 +0900

    #81 [rename] Global -> Common으로 이름을 변경하고 코드의 명확성을 위해 enum이름의 앞에 _을 없앤다.

--- src/main/java/com/dataracy/common/status/CommonSuccessStatus.java
+++ src/main/java/com/dataracy/common/status/CommonSuccessStatus.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum GlobalSuccessStatus implements BaseSuccessCode {
+public enum CommonSuccessStatus implements BaseSuccessCode {
 
     // Global
-    _OK(HttpStatus.OK, "GLOBAL-200", "성공입니다."),
-    _CREATED(HttpStatus.CREATED, "GLOBAL-201", "생성에 성공했습니다."),
-    _NO_CONTENT(HttpStatus.NO_CONTENT, "GLOBAL-204", "성공입니다."),
+    OK(HttpStatus.OK, "GLOBAL-200", "성공입니다."),
+    CREATED(HttpStatus.CREATED, "GLOBAL-201", "생성에 성공했습니다."),
+    NO_CONTENT(HttpStatus.NO_CONTENT, "GLOBAL-204", "성공입니다."),
     ;
 
     private final HttpStatus httpStatus;
