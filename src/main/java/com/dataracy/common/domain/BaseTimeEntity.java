a2db1f0e2f5d7a001a0856fa5e88e1c1e14ea0b3
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 14:52:05 +0900

    #81 [feat] auditing 기능에 nullable=false 속성을 추가합니다.

--- src/main/java/com/dataracy/common/domain/BaseTimeEntity.java
+++ src/main/java/com/dataracy/common/domain/BaseTimeEntity.java
@@ -22,9 +22,10 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
