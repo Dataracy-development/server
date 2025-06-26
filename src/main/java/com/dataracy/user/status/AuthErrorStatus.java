7f934c9bcec77dd28145ef30cebfcf4e2f837030
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 21:18:37 +0900

    #85 [feat] 쿠키 관련 util 메서드를 추가한다.

--- src/main/java/com/dataracy/user/status/AuthErrorStatus.java
+++ src/main/java/com/dataracy/user/status/AuthErrorStatus.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorStatus implements BaseErrorCode {
 
     NOT_FOUND_ACCESS_TOKEN_IN_HEADER(HttpStatus.UNAUTHORIZED, "AUTH-001", "헤더에 어세스토큰이 누락되었습니다."),
+    NOT_FOUND_REFRESH_TOKEN_IN_COOKIES(HttpStatus.UNAUTHORIZED, "AUTH-002", "쿠키에 리프레시토큰이 누락되었습니다."),
     ;
 
     private final HttpStatus httpStatus;
