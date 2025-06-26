e94b6509d2e22910dc1cedee1345165e0efd9d3f
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-26 20:58:13 +0900

    #85 [feat] 파일 설정 관련 공통 메서드를 구현한다.

---
+++ src/main/java/com/dataracy/common/util/FileUtil.java
@@ -0,0 +1,33 @@
+package com.dataracy.common.util;
+
+import com.dataracy.common.status.CommonErrorStatus;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUtil {
+
+    public static void validateProfileImage(MultipartFile file) {
+        if (file == null || file.isEmpty()) return;
+        long maxSize = 10 * 1024 * 1024; // 10MB
+        if (file.getSize() > maxSize) {
+            throw new CommonException(CommonErrorStatus.OVER_MAXIMUM_IMAGE_FILE_SIZE);
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        if (!originalFilename.matches("(?i).+\\.(jpg|jpeg|png)$")) {
+            throw new CommonException(CommonErrorStatus.BAD_REQUEST_IMAGE_FILE_TYPE);
+        }
+    }
+
+    public static void validateGeneralFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) return;
+        long maxSize = 50 * 1024 * 1024; // 50MB
+        if (file.getSize() > maxSize) {
+            throw new CommonException(CommonErrorStatus.OVER_MAXIMUM_FILE_SIZE);
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        if (!originalFilename.matches("(?i).+\\.(jpg|jpeg|png|pdf|docx|xlsx|pptx|txt)$")) {
+            throw new CommonException(CommonErrorStatus.BAD_REQUEST_FILE_TYPE);
+        }
+    }
+}
