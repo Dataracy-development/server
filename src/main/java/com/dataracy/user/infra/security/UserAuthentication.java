4dbfb9dcc36de5c287da42bf886d4defaaa9cc7d
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-27 23:36:51 +0900

    #80 [rename] 유저 인증정보와 관련된 패키지명을 security로 변경한다.

--- src/main/java/com/dataracy/user/infra/security/UserAuthentication.java
+++ src/main/java/com/dataracy/user/infra/security/UserAuthentication.java
@@ -1,4 +1,4 @@
-package com.dataracy.user.infra.auth;
+package com.dataracy.user.infra.security;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
