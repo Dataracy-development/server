5ed00f9a7ddc2be28676e7ca511f0f885c7f2dae
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-28 08:16:27 +0900

    #82 [feat] JwtProperties의 유효성 검사를 추가한다.

--- src/main/java/com/dataracy/modules/auth/infra/jwt/JwtProperties.java
+++ src/main/java/com/dataracy/modules/auth/infra/jwt/JwtProperties.java
@@ -1,19 +1,34 @@
 package com.dataracy.modules.auth.infra.jwt;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@Validated
 @Component
 @ConfigurationProperties(prefix = "spring.jwt")
 public class JwtProperties {
+
+    @NotBlank(message = "암호화를 위한 jwt 시크릿 키를 입력하세요.")
     private String secret;
+
+    @NotBlank(message = "레지스터토큰 유효시간을 작성해주세요.")
     private long registerTokenExpirationTime;
+
+    @NotBlank(message = "어세스토큰 유효시간을 작성해주세요.")
     private long accessTokenExpirationTime;
+
+    @NotBlank(message = "리프레시토큰 유효시간을 작성해주세요.")
     private long refreshTokenExpirationTime;
+
+    @NotBlank(message = "신규유저의 리다이렉트 주소를 작성해주세요.")
     private String redirectOnboarding;
+
+    @NotBlank(message = "기존유저의 리다이렉트 주소를 작성해주세요.")
     private String redirectBase;
 }
