e9f0b6a9ca5e79cddec24a3d834ad463899ddaa8
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-29 18:25:18 +0900

    #82 [fix] 분산락 처리를 위한 코드를 리팩토링한다.

--- src/main/java/com/dataracy/modules/common/lock/LockAcquisitionException.java
+++ src/main/java/com/dataracy/modules/common/lock/LockAcquisitionException.java
@@ -1,7 +1,12 @@
 package com.dataracy.modules.common.lock;
 
 public class LockAcquisitionException extends RuntimeException {
+
     public LockAcquisitionException(String message) {
         super(message);
+    }
+
+    public LockAcquisitionException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
