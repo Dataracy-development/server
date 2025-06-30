4645615fdb41be08aaa27d5af359030ed024879d
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-06-30 10:22:08 +0900

    #92 [feat] UserAuthentication에서 인증 객체 정보 제공 메서드를 추가한다.

---
+++ src/main/java/com/dataracy/modules/security/status/SecurityErrorStatus.java
@@ -0,0 +1,19 @@
+package com.dataracy.modules.security.status;
+
+import com.dataracy.modules.common.status.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum SecurityErrorStatus implements BaseErrorCode {
+
+    UNEXPECTED_PRINCIPAL_TYPE_NOT_USER_DETAILS(HttpStatus.INTERNAL_SERVER_ERROR, "SECURITY-001", "인증 객체에 저장된 인증 객체의 principal가 user details가 아닙니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
