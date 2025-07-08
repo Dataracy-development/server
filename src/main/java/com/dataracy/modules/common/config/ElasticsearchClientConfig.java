25c92482519fc879846e36493086f6c5a7285a5b
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-08 14:12:29 +0900

    #101 [feat] elasticsearch 구현체를 생성하며 elasticsearchClient 설정파일을 생성한다.

---
+++ src/main/java/com/dataracy/modules/common/config/ElasticsearchClientConfig.java
@@ -0,0 +1,27 @@
+package com.dataracy.modules.common.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ElasticsearchClientConfig {
+
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(HttpHost.create(elasticsearchHost)).build();
+        return new ElasticsearchClient(
+                new RestClientTransport(restClient, new JacksonJsonpMapper())
+        );
+    }
+}
