f7c00973ff694a5776525ce21919d80ba0e20316
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-08 15:17:53 +0900

    #101 [fix] elasticsearch 기본 호스트 주소를 추가한다.

--- src/main/java/com/dataracy/modules/behaviorlog/application/port/out/BehaviorLogRepositoryPort.java
+++ src/main/java/com/dataracy/modules/behaviorlog/application/port/out/BehaviorLogRepositoryPort.java
@@ -2,6 +2,9 @@ package com.dataracy.modules.behaviorlog.application.port.out;
 
 import com.dataracy.modules.behaviorlog.domain.model.BehaviorLog;
 
+/**
+ * 행동 로그 저장소 포트
+ */
 public interface BehaviorLogRepositoryPort {
     void save(BehaviorLog log);
 }
