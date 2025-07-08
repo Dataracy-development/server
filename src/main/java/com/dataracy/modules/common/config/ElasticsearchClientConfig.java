2a527001f2c62c666d22dfc1a0764d56489cd563
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-08 15:43:35 +0900

    #101 [fix] elasticSearchConfig 설정에서 연결 풀 설정과 타임 아웃 설정을 추가한다.

--- src/main/java/com/dataracy/modules/common/config/ElasticsearchClientConfig.java
+++ src/main/java/com/dataracy/modules/common/config/ElasticsearchClientConfig.java
@@ -17,9 +17,20 @@ public class ElasticsearchClientConfig {
     @Value("${elasticsearch.host:http://localhost:9200}")
     private String elasticsearchHost;
 
+    /**
+     * 연결 설정 및 타임 아웃 설정을 추가한다.
+     */
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(HttpHost.create(elasticsearchHost)).build();
+        RestClient restClient = RestClient.builder(HttpHost.create(elasticsearchHost))
+                .setRequestConfigCallback(requestConfigBuilder ->
+                        requestConfigBuilder
+                                .setConnectTimeout(5000)
+                                .setSocketTimeout(60000))
+                .setHttpClientConfigCallback(httpClientBuilder ->
+                        httpClientBuilder.setMaxConnTotal(100)
+                                .setMaxConnPerRoute(10))
+                .build();
         return new ElasticsearchClient(
                 new RestClientTransport(restClient, new JacksonJsonpMapper())
         );
