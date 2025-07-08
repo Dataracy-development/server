9f58b5b8257c0aab4d72d7dcbb62c9d719263dd3
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-08 14:31:24 +0900

    #101 [feat] 카프카 익명 로그 전송 구현체 구현

---
+++ src/main/java/com/dataracy/modules/behaviorlog/adapter/message/kafka/BehaviorLogKafkaProducer.java
@@ -0,0 +1,26 @@
+package com.dataracy.modules.behaviorlog.adapter.message.kafka;
+
+import com.dataracy.modules.behaviorlog.application.port.out.BehaviorLogSendProducerPort;
+import com.dataracy.modules.behaviorlog.domain.model.BehaviorLog;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BehaviorLogKafkaProducer implements BehaviorLogSendProducerPort {
+
+    private final KafkaTemplate<String, BehaviorLog> kafkaTemplate;
+
+    // 토픽 이름
+    private static final String TOPIC = "behavior-logs";
+
+    @Override
+    public void send(BehaviorLog behaviorLog) {
+        // 키: 익명 ID
+        kafkaTemplate.send(TOPIC, behaviorLog.getAnonymousId(), behaviorLog);
+        log.debug("[Kafka Producer] 행동 로그 전송됨 - {}", behaviorLog);
+    }
+}
