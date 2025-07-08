78c722d5ca29901c758e89f93d19578f0bb8225b
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-08 16:55:35 +0900

    #101 [fix] 카프카 설정을 yml파일에서 주입받도록 변경한다.

--- src/main/java/com/dataracy/modules/behaviorlog/adapter/message/kafka/BehaviorLogKafkaProducer.java
+++ src/main/java/com/dataracy/modules/behaviorlog/adapter/message/kafka/BehaviorLogKafkaProducer.java
@@ -4,6 +4,7 @@ import com.dataracy.modules.behaviorlog.application.port.out.BehaviorLogSendProd
 import com.dataracy.modules.behaviorlog.domain.model.BehaviorLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +15,13 @@ public class BehaviorLogKafkaProducer implements BehaviorLogSendProducerPort {
     private final KafkaTemplate<String, BehaviorLog> kafkaTemplate;
 
     // 토픽 이름
-    private static final String TOPIC = "behavior-logs";
+    @Value("${spring.kafka.behavior-log.topic:behavior-logs}")
+    private String topic;
 
     @Override
     public void send(BehaviorLog behaviorLog) {
         // 키: 익명 ID
-        kafkaTemplate.send(TOPIC, behaviorLog.getAnonymousId(), behaviorLog);
+        kafkaTemplate.send(topic, behaviorLog.getAnonymousId(), behaviorLog);
         log.debug("[Kafka Producer] 행동 로그 전송됨 - {}", behaviorLog);
     }
 }
