412f37b7281833f7c96220ca3c8bef50f5d9b7f0
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-10 05:30:09 +0900

    #109 [feat] 익명-실제 유저 병합 레디스에서 조회를 구현한다.

--- src/main/java/com/dataracy/modules/behaviorlog/application/port/out/BehaviorLogMergePort.java
+++ src/main/java/com/dataracy/modules/behaviorlog/application/port/out/BehaviorLogMergePort.java
@@ -1,8 +1,9 @@
 package com.dataracy.modules.behaviorlog.application.port.out;
 
 /**
- * 행동 로그 병합 포트
+ * 레디스 행동 로그 병합 포트
  */
 public interface BehaviorLogMergePort {
     void merge(String anonymousId, Long userId);
+    String findMergedUserId(String anonymousId);
 }
