2150d9d009b8d3dc1f0870623901832a78370ae5
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-09 20:55:59 +0900

    #109 [feat] 클릭, 이동 시를 인식할 수 있도록 어노테이션을 생성한다.

---
+++ src/main/java/com/dataracy/modules/behaviorlog/support/annotation/TrackClick.java
@@ -0,0 +1,9 @@
+package com.dataracy.modules.behaviorlog.support.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TrackClick {
+}
