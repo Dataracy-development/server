1c040c335c8305b586cef72edcc40fe00719909e
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-10 23:20:31 +0900

    #109 [refact] 트랙 추적 어노테이션 및 mdcKey를 리팩토링한다.

--- src/main/java/com/dataracy/modules/behaviorlog/support/annotation/TrackClick.java
+++ src/main/java/com/dataracy/modules/behaviorlog/support/annotation/TrackClick.java
@@ -2,6 +2,9 @@ package com.dataracy.modules.behaviorlog.support.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 사용자의 클릭 행동 추적용 AOP 어노테이션
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- src/main/java/com/dataracy/modules/behaviorlog/support/mdc/MdcKey.java
+++ src/main/java/com/dataracy/modules/behaviorlog/support/mdc/MdcKey.java
@@ -1,20 +1,23 @@
 package com.dataracy.modules.behaviorlog.support.mdc;
 
 /**
- * MDC(Mapped Diagnostic Context)에서 사용하는 키들을 상수로 정의합니다.
- * 로그백(logback) 설정에서 이 key들을 기반으로 로그 템플릿에 값을 삽입할 수 있습니다.
+ * MDC 키 상수 정의
  */
 public class MdcKey {
-    public static final String REQUEST_ID = "requestId";     // 요청 ID
-    public static final String SESSION_ID = "sessionId";     // 세션 ID
-    public static final String USER_ID = "userId";           // 로그인 유저 ID
-    public static final String ANONYMOUS_ID = "anonymousId"; // 익명 유저 ID
-    public static final String ACTION = "action"; // 익명 유저 ID
-    public static final String IP = "ip"; // 클라이언트 ip
-    public static final String PATH = "path"; // 경로
-    public static final String METHOD = "method"; // http 메서드
-    public static final String DB_LATENCY = "dbLatency"; // http 메서드
-    public static final String REFERRER = "referrer";
-    public static final String STAY_TIME = "stayTime";
-    public static final String NEXT_PATH = "nextPath";
+
+    public static final String REQUEST_ID     = "requestId";
+    public static final String SESSION_ID     = "sessionId";
+    public static final String USER_ID        = "userId";
+    public static final String ANONYMOUS_ID   = "anonymousId";
+    public static final String ACTION         = "action";
+    public static final String IP             = "ip";
+    public static final String PATH           = "path";
+    public static final String METHOD         = "method";
+    public static final String DB_LATENCY     = "dbLatency";
+    public static final String REFERRER       = "referrer";
+    public static final String STAY_TIME      = "stayTime";
+    public static final String NEXT_PATH      = "nextPath";
+
+    private MdcKey() {
+    }
 }
