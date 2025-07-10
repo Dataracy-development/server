00fc5107a8dd730ae3ecd3055a8f685a3f391936
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-11 00:00:39 +0900

    #109 [refact]  elasticsearch 클라이언트 설정 파일을 리팩토링한다.

--- src/main/java/com/dataracy/modules/common/config/ElasticsearchClientConfig.java
+++ src/main/java/com/dataracy/modules/common/config/ElasticsearchClientConfig.java
@@ -5,7 +5,6 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.dataracy.modules.behaviorlog.adapter.elasticsearch.ElasticsearchConnectionProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
@@ -15,31 +14,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class ElasticsearchClientConfig {
-    private final ElasticsearchConnectionProperties elasticsearchProperties;
+
+    private final ElasticsearchConnectionProperties properties;
+    private final ObjectMapper objectMapper;
 
     @Bean(destroyMethod = "close")
     public RestClient restClient() {
-        return RestClient.builder(
-                        new HttpHost(
-                                elasticsearchProperties.getHost(),
-                                Integer.parseInt(elasticsearchProperties.getPort()),
-                                elasticsearchProperties.getProtocol()))
-                .setRequestConfigCallback(requestConfigBuilder ->
-                        requestConfigBuilder
-                                .setConnectTimeout(5000)
-                                .setSocketTimeout(60000))
-                .setHttpClientConfigCallback(httpClientBuilder ->
-                        httpClientBuilder.setMaxConnTotal(100)
-                                .setMaxConnPerRoute(10))
+        return RestClient.builder(new HttpHost(properties.getHost(), properties.getPort(), properties.getProtocol()))
+                .setRequestConfigCallback(requestConfig -> requestConfig
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(60000))
+                .setHttpClientConfigCallback(httpClient -> httpClient
+                        .setMaxConnTotal(100)
+                        .setMaxConnPerRoute(10))
                 .build();
     }
 
     @Bean
     public ElasticsearchClient elasticsearchClient(RestClient restClient) {
-        // ✅ JavaTimeModule 등록
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
         JacksonJsonpMapper jsonpMapper = new JacksonJsonpMapper(objectMapper);
         return new ElasticsearchClient(new RestClientTransport(restClient, jsonpMapper));
     }
