d7740616ad4c56c665355bd589e6fa198753074b
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-12 00:19:36 +0900

    #112 [fix] s3 config 파일에 대한 예외처리를 추가하여 변경한다.

--- src/main/java/com/dataracy/modules/common/config/AwsS3Config.java
+++ src/main/java/com/dataracy/modules/common/config/AwsS3Config.java
@@ -5,10 +5,12 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class AwsS3Config {
@@ -24,10 +26,15 @@ public class AwsS3Config {
 
     @Bean
     public AmazonS3 amazonS3() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .build();
+        try {
+            BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            return AmazonS3ClientBuilder.standard()
+                    .withRegion(region)
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .build();
+            } catch (Exception e) {
+                log.error("AWS S3 클라이언트 생성 실패", e);
+                throw new IllegalStateException("AWS S3 클라이언트를 생성할 수 없습니다.", e);
+            }
     }
 }
