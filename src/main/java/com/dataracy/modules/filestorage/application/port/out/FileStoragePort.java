f76f93aa69c8bd52cdffcf41d891e6799c2bb8b3
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-12 03:30:59 +0900

    #118 [rename] s3 시스템을 image -> file로 이름을 변경한다.

--- src/main/java/com/dataracy/modules/filestorage/application/port/out/FileStoragePort.java
+++ src/main/java/com/dataracy/modules/filestorage/application/port/out/FileStoragePort.java
@@ -1,8 +1,8 @@
-package com.dataracy.modules.imagestorage.application.port.out;
+package com.dataracy.modules.filestorage.application.port.out;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public interface ImageStoragePort {
+public interface FileStoragePort {
     String upload(String directory, MultipartFile file);
     void delete(String fileUrl);
     String getUrl(String key);
--- src/main/java/com/dataracy/modules/filestorage/domain/exception/S3UploadException.java
+++ src/main/java/com/dataracy/modules/filestorage/domain/exception/S3UploadException.java
@@ -1,4 +1,4 @@
-package com.dataracy.modules.imagestorage.domain.exception;
+package com.dataracy.modules.filestorage.domain.exception;
 
 public class S3UploadException extends RuntimeException {
     public S3UploadException(String message, Throwable cause) {
