c3b64dc6ccf2e4236acc4dae27683d7e45d684fc
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-13 06:21:46 +0900

    #127 [rename] 자바 패키지 관례에 따라 패키지 이름에 대문자가 아닌 소문자만 사용한다.

---
+++ src/main/java/com/dataracy/modules/reference/adapter/web/api/analysispurpose/AnalysisPurposeController.java
@@ -0,0 +1,32 @@
+package com.dataracy.modules.reference.adapter.web.api.analysispurpose;
+
+import com.dataracy.modules.common.dto.response.SuccessResponse;
+import com.dataracy.modules.reference.adapter.web.mapper.AnalysisPurposeWebMapper;
+import com.dataracy.modules.reference.adapter.web.response.allview.AllAnalysisPurposesWebResponse;
+import com.dataracy.modules.reference.application.dto.response.allview.AllAnalysisPurposesResponse;
+import com.dataracy.modules.reference.application.port.in.analysispurpose.FindAllAnalysisPurposesUseCase;
+import com.dataracy.modules.reference.domain.status.ReferenceSuccessStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AnalysisPurposeController implements AnalysisPurposeApi {
+    private final AnalysisPurposeWebMapper analysisPurposeWebMapper;
+    private final FindAllAnalysisPurposesUseCase findAllAnalysisPurposesUseCase;
+    /**
+     * 전체 분석 목적 목록을 조회하여 성공 응답으로 반환합니다.
+     *
+     * @return 전체 분석 목적 목록이 포함된 성공 응답 객체
+     */
+    @Override
+    public ResponseEntity<SuccessResponse<AllAnalysisPurposesWebResponse>> allAnalysisPurposes (
+    ) {
+        AllAnalysisPurposesResponse allAnalysisPurposesResponse = findAllAnalysisPurposesUseCase.allAnalysisPurposes();
+        AllAnalysisPurposesWebResponse allAnalysisPurposesWebResponse = analysisPurposeWebMapper.toWebDto(allAnalysisPurposesResponse);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.of(ReferenceSuccessStatus.OK_TOTAL_ANALYSIS_PURPOSE_LIST, allAnalysisPurposesWebResponse));
+    }
+}
---
+++ src/main/java/com/dataracy/modules/reference/adapter/web/api/visitsource/VisitSourceApi.java
@@ -0,0 +1,35 @@
+package com.dataracy.modules.reference.adapter.web.api.visitsource;
+
+import com.dataracy.modules.common.dto.response.SuccessResponse;
+import com.dataracy.modules.reference.adapter.web.response.allview.AllVisitSourcesWebResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Reference", description = "참조 데이터 관련 API")
+@RequestMapping("/api/v1/visit-sources")
+public interface VisitSourceApi {
+    /****
+     * 모든 방문 경로(visit source)의 목록을 조회한다.
+     *
+     * @return 전체 방문 경로 목록이 포함된 성공 응답
+     */
+    @Operation(
+            summary = "전체 방문 경로 리스트를 조회",
+            description = "DB에서 전체 방문 경로 리스트를 조회한다.",
+            security = {}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "전체 방문 경로 리스트 조회",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SuccessResponse.class))),
+    })
+    @GetMapping
+    ResponseEntity<SuccessResponse<AllVisitSourcesWebResponse>> allVisitSources();
+}
