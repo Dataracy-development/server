5cbc7c32321da1261506a35fe8e2df03ef0dff76
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-13 08:00:42 +0900

    #127 [style] reference 도메인에 대한 스웨거 문서를 추가한다.

--- src/main/java/com/dataracy/modules/reference/adapter/web/response/allview/AllAnalysisPurposesWebResponse.java
+++ src/main/java/com/dataracy/modules/reference/adapter/web/response/allview/AllAnalysisPurposesWebResponse.java
@@ -1,6 +1,7 @@
 package com.dataracy.modules.reference.adapter.web.response.allview;
 
 import com.dataracy.modules.reference.adapter.web.response.singleview.AnalysisPurposeWebResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ import java.util.List;
  * AnalysisPurpose 리스트 조회를 위한 웹응답 DTO
  * @param analysisPurposes analysisPurpose 리스트
  */
+@Schema(description = "분석 목적 리스트 조회 응답")
 public record AllAnalysisPurposesWebResponse(List<AnalysisPurposeWebResponse> analysisPurposes) {
 }
--- src/main/java/com/dataracy/modules/reference/adapter/web/response/allview/AllOccupationsWebResponse.java
+++ src/main/java/com/dataracy/modules/reference/adapter/web/response/allview/AllOccupationsWebResponse.java
@@ -1,6 +1,7 @@
 package com.dataracy.modules.reference.adapter.web.response.allview;
 
 import com.dataracy.modules.reference.adapter.web.response.singleview.OccupationWebResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ import java.util.List;
  * occupation 리스트 조회를 위한 웹응답 DTO
  * @param occupations occupation 리스트
  */
+@Schema(description = "직업 리스트 조회 응답")
 public record AllOccupationsWebResponse(List<OccupationWebResponse> occupations) {
 }
