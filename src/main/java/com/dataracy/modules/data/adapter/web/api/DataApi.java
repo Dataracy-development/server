cee9aee85cc06cb8e14dce12fc62d40d85bdab63
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-15 02:02:59 +0900

    #134 [feat] 컨트롤러 웹 계층과 서비스를 연결하여 데이터셋 업로드를 구현한다.

--- src/main/java/com/dataracy/modules/data/adapter/web/api/DataApi.java
+++ src/main/java/com/dataracy/modules/data/adapter/web/api/DataApi.java
@@ -17,34 +17,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "Project", description = "프로젝트 관련 API")
-@RequestMapping("/api/v1/projects")
+@Tag(name = "Data", description = "데이터셋 관련 API")
+@RequestMapping("/api/v1/datasets")
 public interface DataApi {
 
-    /**
-     * 프로젝트 정보를 업로드하여 데이터베이스에 저장한다.
-     *
-     * @param file 프로젝트와 연관된 파일(선택 사항)
-     * @param webRequest 업로드할 프로젝트 정보가 담긴 요청 객체
-     * @return 업로드 성공 시 200 OK와 함께 성공 응답을 반환한다.
-     */
     @Operation(
-            summary = "프로젝트를 업로드한다.",
-            description = "제공받은 웹 요청 DTO의 프로젝트 정보를 통해 프로젝트 정보를 db에 저장한다"
+            summary = "데이터셋을 업로드한다.",
+            description = "제공받은 웹 요청 DTO의 데이터셋 정보를 통해 데이터셋 정보를 db에 저장한다"
     )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "프로젝트 업로드에 성공했습니다.",
+            @ApiResponse(responseCode = "200", description = "데이터셋 업로드에 성공했습니다.",
                     content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = SuccessResponse.class)))
     })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    ResponseEntity<SuccessResponse<Void>> uploadProject(
+    ResponseEntity<SuccessResponse<Void>> uploadData(
             @Parameter(hidden = true)
             @CurrentUserId
             Long userId,
 
-            @RequestPart(value = "file", required = false) MultipartFile file,
-
+            @RequestPart(value = "dataFile") MultipartFile dataFile,
+            @RequestPart(value = "thumbnailFile", required = false) MultipartFile thumbnailFile,
             @RequestPart
             DataUploadWebRequest webRequest
     );
