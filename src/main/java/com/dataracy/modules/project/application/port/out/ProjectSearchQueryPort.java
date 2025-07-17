7dda013f7c475dd3b6e57a75b80bba0f45a3d412
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-18 03:30:29 +0900

    #140 [fix] 기존 프로젝트 검색 로직을 삭제하고 프로젝트 업로드 시 저장한 document를 추가한다.

--- src/main/java/com/dataracy/modules/project/application/port/out/ProjectSearchQueryPort.java
+++
@@ -1,9 +0,0 @@
-package com.dataracy.modules.project.application.port.out;
-
-import com.dataracy.modules.project.domain.model.ProjectSearchResult;
-
-import java.util.List;
-
-public interface ProjectSearchQueryPort {
-    List<ProjectSearchResult> searchProjectByTitleOrUsername(String keyword);
-}
