56192e9f8ef9fd14377bafd843b0e48a4e88a832
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-19 08:33:45 +0900

    #144 [refact] queryDsl 정렬 predicate 구현에서 상수로 정렬값을 관리하고 null입력 시 예외 조건을 추가한다.

--- src/main/java/com/dataracy/modules/project/adapter/persistence/repository/query/predicates/ProjectSortBuilder.java
+++ src/main/java/com/dataracy/modules/project/adapter/persistence/repository/query/predicates/ProjectSortBuilder.java
@@ -5,14 +5,23 @@ import com.querydsl.core.types.OrderSpecifier;
 import static com.dataracy.modules.project.adapter.persistence.entity.QProjectEntity.projectEntity;
 
 public final class ProjectSortBuilder {
+    private ProjectSortBuilder() {}
+
+    private static final String CREATED_DESC = "created_desc";
+    private static final String CREATED_ASC = "created_asc";
+
     public static OrderSpecifier<?>[] fromSortOption(String sort) {
+        if (sort == null || sort.trim().isEmpty()) {
+            return new OrderSpecifier[]{projectEntity.createdAt.desc()};
+        }
+
         String lowerCase = sort.toLowerCase();
         return switch (lowerCase) {
-            case "created_desc" -> new OrderSpecifier[]{projectEntity.createdAt.desc()};
-            case "created_asc" -> new OrderSpecifier[]{projectEntity.createdAt.asc()};
-//            case "LIKE_DESC" -> new OrderSpecifier[]{projectEntity.likeCount.desc()};
-//            case "REVIEW_DESC" -> new OrderSpecifier[]{projectEntity.reviewCount.desc()};
-//            case "VIEWS_DESC" -> new OrderSpecifier[]{projectEntity.viewCount.desc()};
+            case CREATED_DESC -> new OrderSpecifier[]{projectEntity.createdAt.desc()};
+            case CREATED_ASC -> new OrderSpecifier[]{projectEntity.createdAt.asc()};
+//          case "LIKE_DESC" -> new OrderSpecifier[]{projectEntity.likeCount.desc()};
+//          case "REVIEW_DESC" -> new OrderSpecifier[]{projectEntity.reviewCount.desc()};
+//          case "VIEWS_DESC" -> new OrderSpecifier[]{projectEntity.viewCount.desc()};
             default -> new OrderSpecifier[]{projectEntity.createdAt.desc()};
         };
     }
