25709da30e2c7e21bf4ce5f3fa1aedb283bd5528
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-19 05:52:24 +0900

    #144 [refact] 기존 프로젝트id로부터 프로젝트를 조회할때 lazy로딩이던 부모 프로젝트를 함께 가져오는 부분을 fetch join으로 가져와 초기화 에러 문제를 해결한다.

--- src/main/java/com/dataracy/modules/project/adapter/persistence/repository/ProjectJpaRepository.java
+++ src/main/java/com/dataracy/modules/project/adapter/persistence/repository/ProjectJpaRepository.java
@@ -1,7 +1,13 @@
 package com.dataracy.modules.project.adapter.persistence.repository;
 
 import com.dataracy.modules.project.adapter.persistence.entity.ProjectEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProjectJpaRepository extends JpaRepository<ProjectEntity, Long> {
+    @Query("SELECT p FROM ProjectEntity p LEFT JOIN FETCH p.parentProject WHERE p.id = :id")
+    Optional<ProjectEntity> findProjectById(@Param("id") Long id);
 }
