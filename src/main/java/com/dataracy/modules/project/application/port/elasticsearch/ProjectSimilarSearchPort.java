d9582ced9e7e0285bb234f9993b096dba339e1b3
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-20 08:16:13 +0900

    #153 [fix] 코드 리뷰를 바탕으로 리팩토링을 진행한다.

--- src/main/java/com/dataracy/modules/project/application/port/elasticsearch/ProjectSimilarSearchPort.java
+++ src/main/java/com/dataracy/modules/project/application/port/elasticsearch/ProjectSimilarSearchPort.java
@@ -6,7 +6,7 @@ import com.dataracy.modules.project.domain.model.Project;
 import java.util.List;
 
 public interface ProjectSimilarSearchPort {
-    /****
+    /**
  * 주어진 프로젝트와 유사한 프로젝트를 추천합니다.
  *
  * @param project 기준이 되는 프로젝트
