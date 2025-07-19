0889cc84b59d50ce7cc121776c0784d73c325749
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-19 17:03:13 +0900

    #150 [feat] 좋아요, 조회, 댓글에 대한 엔티티를 구현한다.

---
+++ src/main/java/com/dataracy/modules/like/domain/enums/TargetType.java
@@ -0,0 +1,13 @@
+package com.dataracy.modules.like.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 역할 enum
+ */
+@Getter
+@RequiredArgsConstructor
+public enum TargetType {
+    BOARD, COMMENT
+}
---
+++ src/main/java/com/dataracy/modules/view/adapter/persistence/entity/ViewEntity.java
@@ -0,0 +1,35 @@
+package com.dataracy.modules.view.adapter.persistence.entity;
+
+import com.dataracy.modules.common.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "project_view")
+public class ViewEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "view_id")
+    private Long id;
+
+    // 타 어그리거트로 간접참조
+    @Column(name = "projectId", nullable = false)
+    private Long projectId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "anonymous_id")
+    private String anonymousId; // 비회원 식별자 (UUID)
+
+    @Column(name = "ip")
+    private String ip;
+
+    @Column(name = "user_agent")
+    private String userAgent;
+}
