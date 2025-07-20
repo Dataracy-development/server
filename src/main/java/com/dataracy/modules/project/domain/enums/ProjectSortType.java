b34d064bcdda11bfa60d713edb6bd61621c83e9b
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-20 09:39:20 +0900

    #155 [feat] 프로젝트 필터링을 위한 sort enum을 구현한다,

---
+++ src/main/java/com/dataracy/modules/project/domain/enums/ProjectSortType.java
@@ -0,0 +1,10 @@
+package com.dataracy.modules.project.domain.enums;
+
+public enum ProjectSortType {
+    LATEST,
+    OLDEST,
+    MOST_LIKED,
+    MOST_VIEWED,
+    MOST_COMMENTED,
+    LEAST_COMMENTED
+}
