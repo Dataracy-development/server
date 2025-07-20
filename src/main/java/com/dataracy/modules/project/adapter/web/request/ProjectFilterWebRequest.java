144b6528cbdd937bfdcb47f68444f3dbd80b69f0
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-21 00:55:49 +0900

    #155 [refact] 필터링된 프로젝트 목록 조회 시 null값 또는 빈값이 입력되었을때의 조건에 따라 적용한다.

--- src/main/java/com/dataracy/modules/project/adapter/web/request/ProjectFilterWebRequest.java
+++ src/main/java/com/dataracy/modules/project/adapter/web/request/ProjectFilterWebRequest.java
@@ -1,14 +1,13 @@
 package com.dataracy.modules.project.adapter.web.request;
 
-import com.dataracy.modules.project.domain.enums.ProjectSortType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ProjectFilterWebRequest(
         @Schema(description = "검색 키워드 (제목 또는 작성자)", example = "LLM")
         String keyword,
 
-        @Schema(description = "정렬 방식", example = "LATEST")
-        ProjectSortType sortType,
+        @Schema(description = "프로젝트 정렬 방식은 LATEST, OLDEST, MOST_LIKED, MOST_VIEWED, MOST_COMMENTED, LEAST_COMMENTED 만 가능합니다.", example = "LATEST")
+        String sortType,
 
         @Schema(description = "도메인 ID")
         Long topicId,
--- src/main/java/com/dataracy/modules/project/domain/enums/ProjectSortType.java
+++ src/main/java/com/dataracy/modules/project/domain/enums/ProjectSortType.java
@@ -1,10 +1,33 @@
 package com.dataracy.modules.project.domain.enums;
 
+import com.dataracy.modules.project.domain.exception.ProjectException;
+import com.dataracy.modules.project.domain.status.ProjectErrorStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+/**
+ * 프로젝트 정렬 enum
+ */
+@Getter
+@RequiredArgsConstructor
 public enum ProjectSortType {
-    LATEST,
-    OLDEST,
-    MOST_LIKED,
-    MOST_VIEWED,
-    MOST_COMMENTED,
-    LEAST_COMMENTED
+    LATEST("LATEST"),
+    OLDEST("OLDEST"),
+    MOST_LIKED("MOST_LIKED"),
+    MOST_VIEWED("MOST_VIEWED"),
+    MOST_COMMENTED("MOST_COMMENTED"),
+    LEAST_COMMENTED("LEAST_COMMENTED"),
+    ;
+
+    private final String value;
+
+    public static ProjectSortType of(String input) {
+
+        return Arrays.stream(ProjectSortType.values())
+                .filter(type -> type.value.equalsIgnoreCase(input) || type.name().equalsIgnoreCase(input))
+                .findFirst()
+                .orElseThrow(() -> new ProjectException(ProjectErrorStatus.INVALID_PROJECT_SORT_TYPE));
+    }
 }
