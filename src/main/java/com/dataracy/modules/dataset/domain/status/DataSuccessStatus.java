320525225971c3b05652b7925cf9d536d3c0eb13
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-23 02:32:40 +0900

    #171 [feat] 데이터셋 자동완성 기능을 위한 컨트롤러를 추가 구현한다.

--- src/main/java/com/dataracy/modules/dataset/domain/status/DataSuccessStatus.java
+++ src/main/java/com/dataracy/modules/dataset/domain/status/DataSuccessStatus.java
@@ -15,6 +15,7 @@ public enum DataSuccessStatus implements BaseSuccessCode {
     GET_DATA_DETAIL(HttpStatus.OK, "200", "데이터 상세 정보 조회가 완료되었습니다."),
     FIND_FILTERED_DATASETS(HttpStatus.OK, "200", "데이터셋 필터링이 완료되었습니다."),
     GET_RECENT_DATASETS(HttpStatus.OK, "200", "최신 데이터셋 목록 조회가 완료되었습니다."),
+    FIND_REAL_TIME_DATASETS(HttpStatus.OK, "200", "데이터셋 자동완성을 위한 데이터셋 목록을 조회한다."),
     COUNT_DATASETS_GROUP_BY_TOPIC(HttpStatus.OK, "200", "토픽 카테고리별 데이터셋 개수를 조회한다."),
     ;
 
