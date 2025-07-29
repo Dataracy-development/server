c9bf7a4f5284efd114e42baa438da85afbeb759c
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-30 05:19:36 +0900

    #190 [style] 잘못된 주석 타입을 제거한다.

--- src/main/java/com/dataracy/modules/dataset/application/port/out/DataKafkaProducerPort.java
+++ src/main/java/com/dataracy/modules/dataset/application/port/out/DataKafkaProducerPort.java
@@ -4,7 +4,7 @@ package com.dataracy.modules.dataset.application.port.out;
  * 데이터셋 카프카 포트
  */
 public interface DataKafkaProducerPort {
-    /****
+    /**
  * 데이터 업로드 이벤트를 전송합니다.
  *
  * @param dataId 업로드된 데이터의 고유 식별자
