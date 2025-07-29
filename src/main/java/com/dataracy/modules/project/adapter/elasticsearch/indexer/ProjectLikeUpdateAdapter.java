77554d669d68141f5219da0e2585401e217abae9
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-30 05:07:48 +0900

    #190 [fix] elasticssearch 댓글 작성, 좋아요 시 카운트 값 1증가에 대하여 1감소와 같이 통일성있게 코드 스타일을 변경한다.

--- src/main/java/com/dataracy/modules/project/adapter/elasticsearch/indexer/ProjectLikeUpdateAdapter.java
+++ src/main/java/com/dataracy/modules/project/adapter/elasticsearch/indexer/ProjectLikeUpdateAdapter.java
@@ -28,7 +28,13 @@ public class ProjectLikeUpdateAdapter implements ProjectLikeUpdatePort {
                             .script(s -> s
                                     .inline(i -> i
                                             .lang("painless")
-                                            .source("ctx._source.likeCount += 1")
+                                            .source("""
+                                                    if (ctx._source.likeCount == null) {
+                                                        ctx._source.likeCount = 1;
+                                                    } else {
+                                                        ctx._source.likeCount += 1;
+                                                    }
+                                                    """)
                                     )
                             ),
                     ProjectSearchDocument.class
