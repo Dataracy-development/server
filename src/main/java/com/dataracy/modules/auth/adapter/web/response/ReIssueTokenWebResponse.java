ef590990fbaaae69d5cc85f5e037838fa73083f0
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-29 11:30:21 +0900

    #200 [feat] http일경우 쿠키 전달이 되지 않기 때문에 개발용 로그인, 토큰 재발급 api를 추가한다.

---
+++ src/main/java/com/dataracy/modules/auth/adapter/web/response/ReIssueTokenWebResponse.java
@@ -0,0 +1,16 @@
+package com.dataracy.modules.auth.adapter.web.response;
+
+/**
+ * 쿠키의 리프레시 토큰을 통한 토큰 재발급을 위한 도메인 응답 객체
+ *
+ * @param accessToken 새롭게 발급한 어세스토큰
+ * @param refreshToken 새롭게 발급한 리프레시토큰
+ * @param accessTokenExpiration 어세스토큰 만료시간
+ * @param refreshTokenExpiration 리프레시토큰 만료시간
+ */
+public record ReIssueTokenWebResponse(
+        String accessToken,
+        String refreshToken,
+        long accessTokenExpiration,
+        long refreshTokenExpiration
+) {}
