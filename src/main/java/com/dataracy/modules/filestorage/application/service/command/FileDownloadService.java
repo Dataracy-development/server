2fe7487d3eef283e6f77d73a59530ff9c2580005
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-07-31 01:40:28 +0900

    #210 [fix] s3Url에서 preSignedUrl을 생성하는 서비스 메서드의 유효성 검사를 실행한다.

--- src/main/java/com/dataracy/modules/filestorage/application/service/command/FileDownloadService.java
+++ src/main/java/com/dataracy/modules/filestorage/application/service/command/FileDownloadService.java
@@ -3,8 +3,11 @@ package com.dataracy.modules.filestorage.application.service.command;
 import com.dataracy.modules.filestorage.application.port.in.DownloadFileUseCase;
 import com.dataracy.modules.filestorage.application.port.out.FileStoragePort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nl.basjes.shaded.org.springframework.util.StringUtils;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileDownloadService implements DownloadFileUseCase {
@@ -13,6 +16,13 @@ public class FileDownloadService implements DownloadFileUseCase {
 
     @Override
     public String generatePreSignedUrl(String s3Url, int expirationSeconds) {
+        if (!StringUtils.hasText(s3Url)) {
+            throw new IllegalArgumentException("S3 URL은 비어있을 수 없습니다");
+        }
+        if (expirationSeconds <= 0) {
+            throw new IllegalArgumentException("만료 시간은 0보다 커야 합니다");
+        }
+        log.debug("Pre-signed URL 생성 요청: s3Url={}, expirationSeconds={}", s3Url, expirationSeconds);
         return fileStoragePort.getPreSignedUrl(s3Url, expirationSeconds);
     }
 }
