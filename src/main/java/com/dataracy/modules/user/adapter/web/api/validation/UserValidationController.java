2043675cb66cbeed3a031ca8f7d423963b6e5012
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-08-01 19:27:27 +0900

    #216 [fix] 모델뷰를 반환하는 사용하지 않는 컨트롤러를 삭제한다.

--- src/main/java/com/dataracy/modules/user/adapter/web/api/validation/UserValidationController.java
+++ src/main/java/com/dataracy/modules/user/adapter/web/api/validation/UserValidationController.java
@@ -10,8 +10,6 @@ import com.dataracy.modules.user.domain.status.UserSuccessStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
@@ -39,25 +37,5 @@ public class UserValidationController implements UserValidationApi {
         LoggerFactory.api().logResponse("[DuplicateNickname] 닉네임 중복 API 응답 완료", startTime);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.of(UserSuccessStatus.OK_NOT_DUPLICATED_NICKNAME));
-    }
-
-    /**
-     * "/onboarding" 경로에 대한 GET 요청을 처리하여 "onboarding" 뷰 이름을 반환합니다.
-     *
-     * @return "onboarding" 뷰 이름
-     */
-    @GetMapping("/onboarding")
-    public String onboarding(Model model) {
-        return "onboarding";
-    }
-
-    /**
-     * "/base" 경로에 대한 GET 요청을 처리하여 "base" 뷰 이름을 반환합니다.
-     *
-     * @return "base" 뷰 이름
-     */
-    @GetMapping("/base")
-    public String base(Model model) {
-        return "base";
     }
 }
