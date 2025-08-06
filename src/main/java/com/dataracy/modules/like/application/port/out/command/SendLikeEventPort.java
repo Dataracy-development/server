8f14998d1f953cdd9f6896c24c1aef3d63a7120b
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-08-06 10:32:56 +0900

    #229 [fix] 좋아요 처리 시 comment부분인데 project로 설정한 에러를 수정하고, 파라미터를 명확화한다.

--- src/main/java/com/dataracy/modules/like/application/port/out/command/SendLikeEventPort.java
+++ src/main/java/com/dataracy/modules/like/application/port/out/command/SendLikeEventPort.java
@@ -3,5 +3,5 @@ package com.dataracy.modules.like.application.port.out.command;
 import com.dataracy.modules.like.domain.enums.TargetType;
 
 public interface SendLikeEventPort {
-    void sendLikeEvent(TargetType targetType, Long projectId, boolean previouslyLiked);
+    void sendLikeEvent(TargetType targetType, Long targetId, boolean previouslyLiked);
 }
