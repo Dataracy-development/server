95d80063fd5bef5752818123648f78d181500ace
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-08-07 10:44:16 +0900

    #232 [fix] 이메일 유효성 검사에서 default처리를 추가하고 사용하지 않는 Slf4j 애노테이션을 제거한다.

--- src/main/java/com/dataracy/modules/comment/adapter/kafka/producer/CommentKafkaProducerAdapter.java
+++ src/main/java/com/dataracy/modules/comment/adapter/kafka/producer/CommentKafkaProducerAdapter.java
@@ -3,12 +3,10 @@ package com.dataracy.modules.comment.adapter.kafka.producer;
 import com.dataracy.modules.comment.application.port.out.command.event.SendCommentEventPort;
 import com.dataracy.modules.common.logging.support.LoggerFactory;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CommentKafkaProducerAdapter implements SendCommentEventPort {
