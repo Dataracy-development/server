5830740f96b1dc2f38392e1f7b3c3637fa4308f1
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-08-24 23:29:31 +0900

    #253 [feat] comment 도메인의 성능, 부하, 트래픽 테스트를 진행한다.

--- performance-test/common/config.js
+++
@@ -1,6 +0,0 @@
-// k6/common/config.js
-export const BASE_URL = 'http://localhost:8080';
-export const commonHeaders = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-};
