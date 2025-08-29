2417d5bb2a01442fe225f15c6d5d8f871f04003c
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-08-29 17:57:33 +0900

    #259 [rename] 해당 프로젝트와 연결된 데이터셋 찾기의 클래스명과 메서드명을 get에서 find로 변경한다.

--- src/main/java/com/dataracy/modules/dataset/application/port/out/query/read/FindConnectedDataSetsPort.java
+++ src/main/java/com/dataracy/modules/dataset/application/port/out/query/read/FindConnectedDataSetsPort.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface GetConnectedDataSetsPort {
+public interface FindConnectedDataSetsPort {
     /**
      * 특정 프로젝트에 연결된 데이터셋 목록을 페이지네이션하여 조회합니다.
      *
@@ -14,7 +14,7 @@ public interface GetConnectedDataSetsPort {
      * @param pageable 페이지네이션 정보를 담은 객체
      * @return 각 데이터셋과 해당 데이터셋이 연결된 프로젝트 수를 포함하는 페이지 결과
      */
-    Page<DataWithProjectCountDto> getConnectedDataSetsAssociatedWithProject(Long projectId, Pageable pageable);
+    Page<DataWithProjectCountDto> findConnectedDataSetsAssociatedWithProject(Long projectId, Pageable pageable);
 
     /**
      * 주어진 데이터셋 ID 목록에 해당하는 데이터셋과 각 데이터셋에 연결된 프로젝트 수 정보를 반환합니다.
@@ -22,5 +22,5 @@ public interface GetConnectedDataSetsPort {
      * @param ids 조회할 데이터셋의 ID 목록
      * @return 각 데이터셋과 연결된 프로젝트 수 정보를 담은 DataWithProjectCountDto 리스트
      */
-    List<DataWithProjectCountDto> getConnectedDataSetsAssociatedWithProjectByIds(List<Long> ids);
+    List<DataWithProjectCountDto> findConnectedDataSetsAssociatedWithProjectByIds(List<Long> ids);
 }
