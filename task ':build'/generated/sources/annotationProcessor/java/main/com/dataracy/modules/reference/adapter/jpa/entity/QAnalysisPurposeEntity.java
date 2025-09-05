293e670e8a3923cc4b8262c67c0376187175eeb1
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-09-05 16:15:49 +0900

    [chore] https처리를 위한 nginx-upstream 변경

---
+++ task ':build'/generated/sources/annotationProcessor/java/main/com/dataracy/modules/reference/adapter/jpa/entity/QAnalysisPurposeEntity.java
@@ -0,0 +1,41 @@
+package com.dataracy.modules.reference.adapter.jpa.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAnalysisPurposeEntity is a Querydsl query type for AnalysisPurposeEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAnalysisPurposeEntity extends EntityPathBase<AnalysisPurposeEntity> {
+
+    private static final long serialVersionUID = -1730293752L;
+
+    public static final QAnalysisPurposeEntity analysisPurposeEntity = new QAnalysisPurposeEntity("analysisPurposeEntity");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath label = createString("label");
+
+    public final StringPath value = createString("value");
+
+    public QAnalysisPurposeEntity(String variable) {
+        super(AnalysisPurposeEntity.class, forVariable(variable));
+    }
+
+    public QAnalysisPurposeEntity(Path<? extends AnalysisPurposeEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAnalysisPurposeEntity(PathMetadata metadata) {
+        super(AnalysisPurposeEntity.class, metadata);
+    }
+
+}
+
