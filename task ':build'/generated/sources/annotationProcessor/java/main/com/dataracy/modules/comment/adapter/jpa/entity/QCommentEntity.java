90d9fa3f97ae4cbfcc17d09798f5aad7fbe3c692
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-09-05 16:15:49 +0900

    [chore] https처리를 위한 nginx-upstream 변경

---
+++ task ':build'/generated/sources/annotationProcessor/java/main/com/dataracy/modules/comment/adapter/jpa/entity/QCommentEntity.java
@@ -0,0 +1,55 @@
+package com.dataracy.modules.comment.adapter.jpa.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QCommentEntity is a Querydsl query type for CommentEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCommentEntity extends EntityPathBase<CommentEntity> {
+
+    private static final long serialVersionUID = -990383439L;
+
+    public static final QCommentEntity commentEntity = new QCommentEntity("commentEntity");
+
+    public final com.dataracy.modules.common.base.QBaseTimeEntity _super = new com.dataracy.modules.common.base.QBaseTimeEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Long> likeCount = createNumber("likeCount", Long.class);
+
+    public final NumberPath<Long> parentCommentId = createNumber("parentCommentId", Long.class);
+
+    public final NumberPath<Long> projectId = createNumber("projectId", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public final NumberPath<Long> userId = createNumber("userId", Long.class);
+
+    public QCommentEntity(String variable) {
+        super(CommentEntity.class, forVariable(variable));
+    }
+
+    public QCommentEntity(Path<? extends CommentEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QCommentEntity(PathMetadata metadata) {
+        super(CommentEntity.class, metadata);
+    }
+
+}
+
